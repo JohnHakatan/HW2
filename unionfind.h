af79f763f82e4d55e8182a38dad7e7bb4c34f2f4
mhamudeljamal17 <76074991+mhamudeljamal17@users.noreply.github.com>
2022-06-09 10:25:13 +0300

    tested and revised

--- unionfind.h
+++ unionfind.h
@@ -12,8 +12,7 @@ class UnionFind{
      int n;
  
 public:
-    // Constructor to create and
-    // initialize sets of n items
+  
     UnionFind(int n)
     {
        // rank = new int[n];
@@ -22,8 +21,7 @@ public:
         this->n = n;
         makeSet();
     }
- 
-    // Creates n single item sets
+
     void makeSet()
     {
         for (int i = 0; i < n; i++) {
@@ -32,21 +30,16 @@ public:
         }
     }
  
-    // Finds set of given item x
+
     T* findd(int x)
     {
         // Finds the representative of the set
         // that x is an element of
         if (parent[x]->getId() != x) {
  
-            // if x is not the parent of itself
-            // Then x is not the representative of
-            // his set,
+        
             parent[x] = findd(parent[x]->getId());
  
-            // so we recursively call Find on its parent
-            // and move i's node directly under the
-            // representative of this set
         }
  
         return parent[x];
@@ -56,8 +49,6 @@ public:
         return findd(x)->getId();
     }
  
-    // Do union of two sets represented
-    // by x and y.
     void printFamily(int x)
     {
 int id=find(x);
@@ -66,7 +57,7 @@ for(int i=0;i<n;i++)
     if(find(parent[i]->getId())==id)
     cout<<i<<endl;
 }
-//cout<<parent[458]->getId()<<endl;
+
     }
 
 
@@ -153,125 +144,6 @@ if(x==y)return;
         delete[] parent;
     }
 };
-/*
-template<class T>
-class UnionFind{
-    T** groups;
-   //parent pointer must be in T with set and get
-    int current_size;
-    int initial_size;
-
-    public:
-    // 
-    void init()
-    {
-
-        for (int i = 0; i < initial_size; i++)
-        {
-            
-            groups[i] = new T(i,i);//company with id and value = i 
-            
-        }
-        
-    }
-    
-    
-    UnionFind(int n)
-    {
-       
-        groups = new T*[n];
-        current_size=n;
-         initial_size=n;
-           init();
-    }
-   
-     T* find_(T* group,int id)
-    {
-       // if(!group)return nullptr;
-        if (group->getParent()==NULL) {//THERE ARE NO PARENT
-            if(group->getId()!=id)groups[id]->setParent(group);
-            return group; 
-        }
-
-        T* to_rtn=find_(group->getParent(),id);
-       // group->setParent(to_rtn);
-    
-        return to_rtn;
-
-    }
-     T* findd(int id)
-    {
-        if(id > initial_size)//there is no id like that
-        return nullptr;
-
-        if(groups[id]==nullptr)//no group
-        return nullptr;
-        
-        return find_(groups[id],id);
-    }
-    int find(int id)
-    {
-        return findd(id)->getId();
-    }
-    
-    void union_groups(int id1,int id2)
-    {
-        T* t1=findd(id1);
-        T* t2=findd(id2);
-        if( id1==id2||t1->getId()==t2->getId() )
-        return;
-       if(t1->size == t2->size){ if(id2 <id1)
-        {
-               t1->getParent()?t2->setParent(t1->getParent()):t2->setParent(t1);
-              t1->moveEmployees(t2);
-           
-  
-        }else{
-           t2->getParent()?t1->setParent(t2->getParent()):t1->setParent(t2);
-    t2->moveEmployees(t1);
-        }
-        
-       }
-       else if(t1->size > t2->size)
-       { t1->getParent()?t2->setParent(t1->getParent()):t2->setParent(t1);
-           t1->moveEmployees(t2);//move data of t2 to t1 
-                                 //moving trees and updating father and point the moved one to the bigger one
-
-       
-       }else
-       {
-            
-t2->getParent()?t1->setParent(t2->getParent()):t1->setParent(t2);
-           t2->moveEmployees(t1);
-          
-       }
-
-       if(id2 < id1)
-        {
-               t1->getParent() ? t2->setParent(t1->getParent()):t2->setParent(t1);
-          
-               t1->moveEmployees(t2);
-  
-        }else{
-           t2->getParent() ? t1->setParent(t2->getParent()):t1->setParent(t2);
-            t2->moveEmployees(t1);
-       
-        }
-
-    
-
-    }
-
-
-~UnionFind()
-    {
-        for(int i=0;i<current_size;i++)
-        {
-            delete groups[i];
-        }
-     delete groups;
-    }
-};*/
 
 
 
