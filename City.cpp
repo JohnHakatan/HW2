37a02b6b68aba81e74dcaff2970c5c1b44c0c77a
mhamudeljamal17 <76074991+mhamudeljamal17@users.noreply.github.com>
2022-06-13 12:50:15 +0300

    functions added-prmoteEmployee and other related stuff

--- City.cpp
+++ City.cpp
@@ -14,6 +14,8 @@ Company* City::getCompanyById(int id)
 }
 
 
+
+
     StatusType City::CompanyValue( int companyID, void ** standing)
     {
    
@@ -30,44 +32,25 @@ Company* City::getCompanyById(int id)
     {
         shared_ptr<Employee> to_return=allEmployees.find(id)->data;
         return to_return;
-    //     Company** ptr=this->Companies->pointers;
-    //     shared_ptr<Employee> employee_to_check=nullptr;
-    //    while(ptr!=nullptr)
-    //    {
-           
-    //     employee_to_check=(*ptr)->GetEmployee(id);
-    //     ptr++;
-    //    }
-    //    // shared_ptr<Employee>employee_to_check=make_shared<Employee>(id,0,salary);
-    //   //  return this->Companies.find(this->employees_by_salary.(),employee_to_check);
-    //   return employee_to_check;
+ 
     }
 
-   /* void City::updateEmployee(int EmployeeID,int SalaryIncrease,int BumpGrade)
+    void City::updateEmployee(shared_ptr<Employee> em,int EmployeeID,int SalaryIncrease,int BumpGrade)
     {
-        AVLNode<shared_ptr<Employee>,EmployeeComparebyID>* old_data_id=getEmployeeById(EmployeeID);
+        //shared_ptr
         
-        int new_salary=old_data_id->getData()->getSalary()+SalaryIncrease;
-        int new_grade=old_data_id->getData()->getGrade();
+        
+        int new_grade=em->getGrade();
         if(BumpGrade>0)
         {
-            new_grade+=1;
+            new_grade+=BumpGrade;
         }
-        shared_ptr<Employee> new_by_id;
-        try{
-            new_by_id =make_shared<Employee>(EmployeeID,new_grade,new_salary,old_data_id->getData()->getCompanyId(),old_data_id->getData()->getCompany());
-        }catch(std::bad_alloc& e)
-        {
-            throw;
-        }
-        shared_ptr<Employee> ptr=old_data_id->getData();
-        employees_by_salary.AVLRemoveVal(ptr);
-        employees_by_id.AVLRemoveVal(ptr);
-        shared_ptr<Employee> new_by_id1=new_by_id;
-        employees_by_id.AVLInsert(new_by_id);
-        employees_by_salary.AVLInsert(new_by_id1);
+        em->setGrade(new_grade);
+
         
-    }*/
+    
+        
+    }
 
 
     City::City(int k){
@@ -114,24 +97,6 @@ Company* City::getCompanyById(int id)
         sum_of_zero_employees_grade+=Grade;
         num_of_employees++;
 
-        // int val = employees_by_id.AVLInsert(employee_to_check);
-        // if(val==MEMORY_ERROR || val==DATA_EXIST)
-        // {
-        //     return FAILURE;
-        // }
-
-        // val= employees_by_salary.AVLInsert(employee_to_check);
-        //  if(val==MEMORY_ERROR || val==DATA_EXIST)
-        // {//maybe we must delete what we have inserted
-        //     return FAILURE;
-        // }
-   
-        // this->num_of_employees++;
-        // try{
-        // company_to_update->addEmployeeToCompany( employee_to_check);
-        // }catch(std::bad_alloc& e){
-        // throw;
-        // }
         return SUCCESS;
 
      
@@ -152,12 +117,14 @@ Company* City::getCompanyById(int id)
         {
             return FAILURE;
         }
+
         allEmployees.remove(EmployeeID);
         if(employee_to_remove->getSalary()==0)
         {
             employees_with_zero_salary--;
             sum_of_zero_employees_grade-=employee_to_remove->getGrade();
         }
+
         employee_to_remove->getCompany()->RemoveEmployee(EmployeeID);
         employees_by_salary.AVLRemoveVal(employee_to_remove);
         this->num_of_employees--;
@@ -167,9 +134,9 @@ Company* City::getCompanyById(int id)
     
     
 
-    StatusType City::PromoteEmployee( int EmployeeID, int SalaryIncrease, int BumpGrade)
+    StatusType City::PromoteEmployee( int EmployeeID, int BumpGrade)
     {
-        if(EmployeeID<=0 || SalaryIncrease<=0)
+        if(EmployeeID<=0)
         {
             return INVALID_INPUT;
         }
@@ -179,65 +146,20 @@ Company* City::getCompanyById(int id)
         {
             return FAILURE;
         }
-       // cout<<"-----------------------------------------"<<to_update->getData()->getCompanyId()<<endl;
-         Company* company_of_employee=to_update->getCompany();
-      //  cout<<company_of_employee->getData()->getID();
-          try{
-            updateEmployee(EmployeeID,SalaryIncrease,BumpGrade);
-         }catch(std::bad_alloc& e)
-        {
-            return FAILURE;
-        }
-      to_update=getEmployee(EmployeeID);
-        try{
-            company_of_employee->updateEmployee(to_update->getData());
-         }catch(std::bad_alloc& e)
-        {
-            return FAILURE;
-        }
-
+    
+     this->RemoveEmployee(EmployeeID);
+     //to_update->setGrade(to_update->getGrade()+BumpGrade);
+     this->AddEmployee(EmployeeID,to_update->getCompany()->getId(),to_update->getGrade()+BumpGrade);
+     this->EmployeeSalaryIncrease(EmployeeID,to_update->getSalary());
+     
       
 
         return SUCCESS;
     }
 
-    /*StatusType City::HireEmployee(int EmployeeID, int NewCompanyID)
-    {
-        if(EmployeeID <=0 || NewCompanyID <=0)return INVALID_INPUT;
- AVLNode<shared_ptr<Employee>,EmployeeComparebyID>* ptr =getEmployeeById(EmployeeID);
-  if(!ptr)return FAILURE;
-        AVLNode <shared_ptr<Company>,CompanyCompareByID>* ccc= getCompanyById(NewCompanyID);
-        if((ccc==NULL) || ((ptr->getData()->getCompanyId())==NewCompanyID) )return FAILURE;
-         shared_ptr<Company> company_ptr=ccc->getData();
-shared_ptr<Employee>  employee_ptr=make_shared<Employee>(EmployeeID,ptr->getData()->getGrade(),ptr->getData()->getSalary()
-,NewCompanyID,company_ptr);
-     RemoveEmployee(EmployeeID);
-    //  return AddEmployee(EmployeeID,NewCompanyID,)
-     
-  try{
-        int val = employees_by_id.AVLInsert(employee_ptr);
-        if(val==MEMORY_ERROR || val==DATA_EXIST)
-        {
-            return FAILURE;
-        }
-        val= employees_by_salary.AVLInsert(employee_ptr);
-         if(val==MEMORY_ERROR || val==DATA_EXIST)
-        {
-            return FAILURE;
-        }
-      }catch(std::bad_alloc& e)
-        {
-            return ALLOCATION_ERROR;
-        }
-        this->num_of_employees++;
-        try{
-        company_ptr->addEmployeeToCompany(employee_ptr);
-        }catch(std::bad_alloc& e){
-        throw;
-        }
-        return SUCCESS;
-        
-    }*/
+
+
+
 static double find_how_many_lower_than(AVLTree<shared_ptr<Employee>,EmployeeComparebySalary>* tree,AVLNode<shared_ptr<Employee>,EmployeeComparebySalary>* root,int Salary)
 {
 if(!root)return 0;// check this when using the function in avg function
