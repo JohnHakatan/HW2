e5780dbed3dac23f400f349f68517bb9a3a71ee7
mhamudeljamal17 <76074991+mhamudeljamal17@users.noreply.github.com>
2022-06-13 12:50:51 +0300

    Update City.h

--- City.h
+++ City.h
@@ -29,8 +29,8 @@ class City
     Company* getCompanyById(int id);
 //AVLNode<shared_ptr<Employee>,EmployeeComparebyID>* getEmployeeById(int id);
     shared_ptr<Employee> GetEmployee(int id);
-    void updateEmployee(int EmployeeID,int SalaryIncrease,int BumpGrade);
-    StatusType PromoteEmployee( int EmployeeID, int SalaryIncrease, int BumpGrade);
+    void updateEmployee(shared_ptr<Employee> em,int EmployeeID,int SalaryIncrease,int BumpGrade);
+    StatusType PromoteEmployee( int EmployeeID, int BumpGrade);
     Company* getOriginalCompany(int id);
     StatusType AddEmployee(int EmployeeID, int CompanyID, int Grade);
     StatusType RemoveEmployee(int EmployeeID);
@@ -38,7 +38,7 @@ class City
     StatusType SumOfBumpGradeBetweenTopWorkersByGroup( int companyID, int m);
     
     StatusType AverageBumpGradeBetweenSalaryByGroup(int companyID, int lowerSalary, int higherSalary);
-    StatusType CompanyValue( int companyID);
+    StatusType CompanyValue( int companyID, void ** standing);
     StatusType AcquireCompany(int AcquirerID, int TargetID, double Factor);
     StatusType BumpGradeToEmployees(int lowerSalary, int higherSalary, int bumpGrade);
     int SumbumpGradeFromTree(AVLTree<shared_ptr<Employee>,EmployeeComparebySalary>* tree ,AVLNode<shared_ptr<Employee>,EmployeeComparebySalary>* root,int m);
